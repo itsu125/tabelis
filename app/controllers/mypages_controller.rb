23e6b5e0e6f5b93c5afa7b8b8fc822d460b064ba
itsu125 <iikura.20250908@gmail.com>
2025-11-25 01:27:05 +0900

    マイページの項目追加

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,6 +3,11 @@ class MypagesController < ApplicationController
 
   def show
     @user = current_user
-    @shops = current_user.shops.order(created_at: :desc)
+    # 行きたい/行った の統計
+    @want_count = current_user.shops.where(status: "want").count
+    @went_count = current_user.shops.where(status: "went").count
+
+    # 最近追加したショップ 3件
+    @recent_shops = current_user.shops.order(created_at: :desc).limit(3)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   # アソシエーション
   has_many :shops, dependent: :destroy
+  has_one_attached :icon
 
   # バリデーション
   validates :name, presence: true, length: { maximum: 30 }
