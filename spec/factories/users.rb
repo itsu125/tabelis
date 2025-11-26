ca8123e1a572cd77a3b7e91061baa24db7c67a08
itsu125 <iikura.20250908@gmail.com>
2025-11-27 02:13:26 +0900

    userモデル単体テスト（FactoryBot含む）

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name}
+    sequence(:email) { |n| "user#{n}@example.com"}
+
+    password              { "password" }
+    password_confirmation { password }
+  end
+end
