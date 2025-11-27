97a395c4c98e5691507a718522137ba065f37e83
itsu125 <iikura.20250908@gmail.com>
2025-11-28 06:42:27 +0900

    Update tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,27 +4,17 @@ module.exports = {
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
   ],
+  safelist: [
+    "bg-tag-hitori",
+    "bg-tag-friends",
+    "bg-tag-family",
+    "bg-tag-memory",
+    "bg-tag-lunch",
+    "bg-tag-dinner",
+    "bg-tag-takeout",
+  ],
   theme: {
-    extend: {
-      safelist: [
-        "bg-tag-hitori",
-        "bg-tag-friends",
-        "bg-tag-family",
-        "bg-tag-memory",
-        "bg-tag-lunch",
-        "bg-tag-dinner",
-        "bg-tag-takeout",
-
-        // ロゴ画像
-        "w-16", "h-16",
-
-        // 共通ヘッダー
-        "w-10", "h-10",
-
-        // マイページアイコン
-        "w-24", "h-24",
-      ],
-    },
+    extend: {},
   },
   plugins: [],
 }
