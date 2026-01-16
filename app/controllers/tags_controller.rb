1f9dfa4b51255b7f2e071a0f5229a6f5f6c4f63d
itsu125 <iikura.20250908@gmail.com>
2026-01-17 01:53:12 +0900

    コントローラー追記（tags）

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,32 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @tag = Tag.new
   end
+
   def create
+    @tag = current_user.tags.build(tag_params)
+
+    if @tag.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html do
+          redirect_back fallback_location: new_shops_path,
+                        notice: "タグを追加しました"
+        end
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { render :new, status: :unprocessable_entity }
+        format.html         { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name, :color_class)
   end
 end
