81f8080b88b017276824c42cb0db574e313440be
itsu125 <iikura.20250908@gmail.com>
2026-01-17 01:56:52 +0900

    モデル追記（タグカラー選択のため）

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,4 +9,16 @@ class Tag < ApplicationRecord
   def self.available_for(user)
     where(user_id: nil).or(where(user_id: user.id))
   end
+
+  def self.color_options
+    [
+      ["グリーン系", "bg-tag-hitori"],
+      ["イエロー系", "bg-tag-friends"],
+      ["ピンク系", "bg-tag-family"],
+      ["パープル系", "bg-tag-memory"],
+      ["ライトブルー系", "bg-tag-lunch"],
+      ["ブルー系", "bg-tag-dinner"],
+      ["サーモン系", "bg-tag-takeout"],
+    ]
+  end
 end
